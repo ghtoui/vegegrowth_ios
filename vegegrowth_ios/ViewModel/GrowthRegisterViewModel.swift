7b4d99a10fc132f8f695ca3c675f0e8d459f2dcb
ghtoui <92294389+ghtoui@users.noreply.github.com>
2023-06-22 12:29:06 +0900

    rxswiftを使ってリファクタリングをした。 #10 MVVMを考えて、View --> ViewModel, ViewModel --> Viewと処理するように 変数名の書き換えも行った Viewでカメラ処理がまだ未完成なので、要検討。Rxswiftのリファレンス通りにしたが、実機での動作は試せていない

--- vegegrowth_ios/ViewModel/GrowthRegisterViewModel.swift
+++ vegegrowth_ios/ViewModel/GrowthRegisterViewModel.swift
@@ -6,12 +6,23 @@
 //
 
 import Foundation
+import RxCocoa
+import RxSwift
+import UIKit.UIImage
 
 protocol GrowthRegisterViewModelInputs {
-    
+    var takePicButtonTapped: PublishRelay<Void> { get }
+    var moveToManagerButtonTapped: PublishRelay<Void> { get }
+    var registerButtonTapped: PublishRelay<Void> { get }
+    var vegeText: BehaviorRelay<String> { get }
+    var isRegisterButton: PublishRelay<Bool> { get }
 }
 
 protocol GrowthRegisterViewModelOutputs {
+    var vegeId: PublishRelay<String> { get }
+    var isPicTake: Driver<Bool> { get }
+    var showAlert: PublishRelay<Void> { get }
+    var cameraAlert: PublishRelay<Void> { get }
 }
 
 protocol GrowthRegisterViewModelType {
@@ -20,10 +31,67 @@ protocol GrowthRegisterViewModelType {
 }
 
 class GrowthRegisterViewModel: GrowthRegisterViewModelType, GrowthRegisterViewModelInputs, GrowthRegisterViewModelOutputs {
+    
     var inputs: GrowthRegisterViewModelInputs { return self }
     var outputs: GrowthRegisterViewModelOutputs { return self }
     
+    // MARK: - inputs
+    var takePicButtonTapped = PublishRelay<Void>()
+    var moveToManagerButtonTapped = PublishRelay<Void>()
+    var registerButtonTapped = PublishRelay<Void>()
+    var vegeText = BehaviorRelay<String>(value: "")
+    var isRegisterButton = PublishRelay<Bool>()
+    
+    // MARK: - outputs
+    var vegeId = PublishRelay<String>()
+    var isPicTake: Driver<Bool>
+    var showAlert = PublishRelay<Void>()
+    var cameraAlert = PublishRelay<Void>()
+    
+    private let vegeManager = VegemanagerClass()
+    private let disposeBag = DisposeBag()
+    private var imgClass: ImgClass!
+    private var graphClass: GraphClass!
+    private var vegeTextList: [String]!
+    
     init() {
+        isPicTake = isRegisterButton.asDriver(onErrorDriveWith: .empty())
+        // vegeTextの値に基づいて、VegeIdが決まる
+        // 野菜の名前に紐づけられたユニークId
+        vegeText
+            .compactMap { [weak self] vegeText in
+                guard let vegeIdDict = self?.vegeManager.getVegeIdDict() else {
+                    return nil
+                }
+                return vegeIdDict[vegeText]
+            }
+            .bind(to: vegeId)
+            .disposed(by: disposeBag)
+
+        
+        // vegeIdが更新されたら、imgclassとgraphclassを生成する
+        // vegeIdに対応したvegeTextListも持ってくる
+        vegeId
+            .subscribe(onNext: { [weak self] vegeId in
+                self?.imgClass = ImgClass(vege_id: vegeId)
+                self?.graphClass = GraphClass(vege_id: vegeId)
+            })
+            .disposed(by: disposeBag)
+        
+        // 登録ボタンが押されたことをViewに通知する
+        registerButtonTapped
+            .subscribe(onNext: { [weak self] in
+                self?.showAlert.accept(())
+            })
+            .disposed(by: disposeBag)
+        
+        // 撮影ボタンが押されたことをViewに通知する
+        takePicButtonTapped
+            .subscribe(onNext: { [weak self] in
+                self?.cameraAlert.accept(())
+            })
+            .disposed(by: disposeBag)
+        
         
     }
 }
--- vegegrowth_ios/Views/GrowthRegisterViewController.swift
+++ vegegrowth_ios/Views/GrowthRegisterViewController.swift
@@ -6,80 +6,136 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
+class GrowthRegisterViewController: UIViewController {
 
-class GrowthRegisterViewController: UIViewController,
-                                UIImagePickerControllerDelegate,
-                                UINavigationControllerDelegate {
-
-    @IBOutlet weak var imgview: UIImageView!
-    @IBOutlet weak var register_button: UIButton!
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var moveToManagerButton: UIBarButtonItem!
+    @IBOutlet weak var takePicButton: UIButton!
+    @IBOutlet weak var registerButton: UIButton!
     
     private var vegeText :String!
-    private var vege_id: String!
     private let viewModel: GrowthRegisterViewModelType = GrowthRegisterViewModel()
+    private let disposeBag = DisposeBag()
     
     // クラス
-    private var img_class: ImgClass!
-    private var graph_class : GraphClass!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         
-        // クラスの生成
-        // 野菜の名前に紐づけられたユニークIDを取得する
-        vege_id = vege_id_dict[vegeText]!
+        viewModel.inputs.isRegisterButton.accept(false)
         
-        img_class = ImgClass(vege_id: vege_id)
-        graph_class = GraphClass(vege_id: vege_id)
-        
-        // vege_text_listの取得
-        if UserDefaults.standard.object(forKey: vege_id) != nil {
-            vege_text_list = UserDefaults.standard.object(forKey: vege_id) as! [String]
-        }
+        takePicButton.rx.tap
+            .flatMapLatest { [weak self] _ in
+                return UIImagePickerController.rx.createWithParent(self) { picker in
+                    picker.sourceType = .camera
+                    picker.allowsEditing = false
+                }
+                .flatMap { $0.rx.didFinishPickingMediaWithInfo }
+                .take(1)
+            }
+            .map { info in
+                return info[.originalImage] as? UIImage
+            }
+            .bind(to: imgView.rx.image)
+            .disposed(by: disposeBag)
+    
+        registerButton.isHidden = true
+        bind()
+    }
 
+    private func bind() {
+        
+        takePicButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+
+        // 登録ボタンの表示切り替え
+        viewModel.outputs.isPicTake
+            .drive(onNext: { [weak self] isPic in
+                if isPic {
+                    self?.registerButton.isHidden = false
+                    print(false)
+                } else {
+                    self?.registerButton.isHidden = true
+                    print(true)
+                }
+            })
+            .disposed(by: disposeBag)
+        
+        // 写真撮影ボタンを押した時の処理
+        takePicButton.rx.tap
+            .bind(to: viewModel.inputs.takePicButtonTapped)
+            .disposed(by: disposeBag)
+        
+//        viewModel.outputs.cameraAlert
+//            .subscribe(onNext: { [weak self] in
+//                self?.takeButtonTapped()
+//            })
+//            .disposed(by: disposeBag)
+        
+        // 登録ボタンを押した時の処理
+        registerButton.rx.tap
+            .bind(to: viewModel.inputs.registerButtonTapped)
+            .disposed(by: disposeBag)
+        
+        // 登録ウィンドウを表示する
+        viewModel.outputs.showAlert
+            .subscribe(onNext: { [weak self] in
+                self?.registerVegeMeasure()
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    // 画面遷移してくる時用
+    // 本当はinitでできると良いが...
+    public func setVegeText(vegeText: String) {
+        print(vegeText)
         // タイトル変更(タップしたラベルを反映する)
         // navigationcontrollerを使っている
         navigationItem.title = vegeText
-        register_button.isHidden = true
-    }
-    
-    public func setVegeText(vegeText: String) {
-        self.vegeText = vegeText
+        viewModel.inputs.vegeText.accept(vegeText)
     }
 
-    // 撮影ボタンが押された時の処理
-    @IBAction func takebutton_click(_ sender: Any) {
-        if UIImagePickerController.isSourceTypeAvailable(.camera) {
-            let image_picker = UIImagePickerController()
-            image_picker.sourceType = .camera
-            image_picker.delegate = self
-            self.present(image_picker, animated: true, completion: nil)
-        }
-    }
+    // 撮影処理
+//    private func takeButtonTapped() {
+//        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+//            let image_picker = UIImagePickerController()
+//            image_picker.sourceType = .camera
+//            image_picker.delegate = self
+//            self.present(image_picker, animated: true, completion: nil)
+//        }
+//    }
     
     // 写真撮影処理
     // takebutton_clickが押されたら処理する
     func imagePickerController(_ picker: UIImagePickerController,
                              didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
-        let take_img = img_class.take_img(picker, didFinishPickingMediaWithInfo: info)
-        imgview.image = take_img
-        register_button.isHidden = false
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "graphviewsegue" {
-            if let destinationVC = segue.destination as? GrowthManageViewController {
-                destinationVC.vege_text = vegeText
-            }
+//        let take_img = img_class.take_img(picker, didFinishPickingMediaWithInfo: info)
+//        imgview.image = take_img
+        picker.dismiss(animated: true)
+
+        guard let img = info[.originalImage] as? UIImage else {
+            print("画像が見つかりませんでした")
+            return
         }
+        // 撮った写真を保存する
+//        vege_text_list = get_vege_text_list(vege_id: vege_id)
+//        let num = vege_text_list.count
+//        file_name = self.vege_id + "_" + String(num)
+//        print(file_name)
+        // 画像の回転を直す
+//        fix_rotateimg = fix_rotate(img: img)!
+//        vege_text_list = get_vege_text_list(vege_id: vege_id)
+//        print(vege_text_list)
+//        registerButton.isHidden = false
     }
     
     // 野菜の大きさを入力する
     // 数字のみ受け付けるように
-    @IBAction func click_registerbutton(_ sender: Any) {
+    private func registerVegeMeasure() {
         var textfield = UITextField()
         
         let alert = UIAlertController(title: "撮影した野菜の大きさを入力してください",
@@ -88,13 +144,13 @@ class GrowthRegisterViewController: UIViewController,
 
         // 登録処理
         let register_action = UIAlertAction(title: "登録",style: .default) { (action) in
-            self.graph_class.add_vege_length(length: Double(textfield.text!)!)
-            let take_img: UIImage = self.img_class.get_takeimg()
-            let file_name = self.img_class.get_filename()
+//            self.graph_class.add_vege_length(length: Double(textfield.text!)!)
+//            let take_img: UIImage = self.img_class.get_takeimg()
+//            let file_name = self.img_class.get_filename()
             // 撮影した画像を保存
-            self.img_class.save_img(file_name: file_name, img: take_img)
+//            self.img_class.save_img(file_name: file_name, img: take_img)
             // 登録したら、ボタンを消す
-            self.register_button.isHidden = true
+            self.registerButton.isHidden = true
         }
         // キャンセル処理, 何もしない
         let cancel_action = UIAlertAction(title: "キャンセル", style: .cancel) { (action) in
@@ -119,4 +175,85 @@ class GrowthRegisterViewController: UIViewController,
         // Pass the selected object to the new view controller.
     }
     */
+}
+
+func dismissViewController(_ viewController: UIViewController, animated: Bool) {
+    if viewController.isBeingDismissed || viewController.isBeingPresented {
+        DispatchQueue.main.async {
+            dismissViewController(viewController, animated: animated)
+        }
+
+        return
+    }
+
+    if viewController.presentingViewController != nil {
+        viewController.dismiss(animated: animated, completion: nil)
+    }
+}
+
+extension Reactive where Base: UIImagePickerController {
+    static func createWithParent(_ parent: UIViewController?, animated: Bool = true, configureImagePicker: @escaping (UIImagePickerController) throws -> Void = { x in }) -> Observable<UIImagePickerController> {
+        return Observable.create { [weak parent] observer in
+            let imagePicker = UIImagePickerController()
+            let dismissDisposable = imagePicker.rx
+                .didCancel
+                .subscribe(onNext: { [weak imagePicker] _ in
+                    guard let imagePicker = imagePicker else {
+                        return
+                    }
+                    dismissViewController(imagePicker, animated: animated)
+                })
+            
+            do {
+                try configureImagePicker(imagePicker)
+            }
+            catch let error {
+                observer.on(.error(error))
+                return Disposables.create()
+            }
+
+            guard let parent = parent else {
+                observer.on(.completed)
+                return Disposables.create()
+            }
+
+            parent.present(imagePicker, animated: animated, completion: nil)
+            observer.on(.next(imagePicker))
+            
+            return Disposables.create(dismissDisposable, Disposables.create {
+                    dismissViewController(imagePicker, animated: animated)
+                })
+        }
+    }
+}
+
+extension Reactive where Base: UIImagePickerController {
+    /**
+     Reactive wrapper for `delegate` message.
+     */
+    public var didFinishPickingMediaWithInfo: Observable<[UIImagePickerController.InfoKey : AnyObject]> {
+        return delegate
+            .methodInvoked(#selector(UIImagePickerControllerDelegate.imagePickerController(_:didFinishPickingMediaWithInfo:)))
+            .map({ (a) in
+                return try castOrThrow(Dictionary<UIImagePickerController.InfoKey, AnyObject>.self, a[1])
+            })
+    }
+
+    /**
+     Reactive wrapper for `delegate` message.
+     */
+    public var didCancel: Observable<()> {
+        return delegate
+            .methodInvoked(#selector(UIImagePickerControllerDelegate.imagePickerControllerDidCancel(_:)))
+            .map {_ in () }
+    }
+    
+}
+    
+private func castOrThrow<T>(_ resultType: T.Type, _ object: Any) throws -> T {
+    guard let returnValue = object as? T else {
+        throw RxCocoaError.castingError(object: object, targetType: resultType)
+    }
+
+    return returnValue
 }
