ff56014463f0cc01e3a50643f8d20b8d0aa0d304
ghtoui <92294389+ghtoui@users.noreply.github.com>
2023-07-16 18:24:08 +0900

    テストデータの作成 #22

--- APIdir/app/route.py
+++ APIdir/app/route.py
@@ -1,16 +1,50 @@
 from flask import jsonify, request
 from app import app
 import time
+import datetime
+
+"""
+struct VegeLengthObject {
+    var date: String
+    var vegeLength: Double
+    var x: Double
+    var memoText: String
+}
+"""
+
+# testデータ作成
+date = datetime.date(2023, 7, 1)
+data_length = 5
+date_format = "%Y年%m月%d日%H時"
+
+name_list = ['レタス', 'キャベツ', 'きゅうり', 'なすび']
+date_list = []
+vegeLength_list = []
+x_list = []
+memoText_list = []
+for i in range(data_length):
+    date_list.append(date.strftime(date_format))
+    vegeLength_list.append(float(i + 1.5))
+    x_list.append(float(i))
+    memoText_list.append("")
+    date += datetime.timedelta(days = 1)
+
+test_data = []
+for i in range(len(name_list)):
+    data = {'name': name_list[i],
+            'date': date_list,
+            'vegeLength': vegeLength_list,
+            'x': x_list,
+            'memoText': memoText_list
+            }
+    test_data.append(data)
 
 # GETリクエストに対応するエンドポイント
 @app.route('/api/data', methods = ['GET'])
 def get_data():
-    data = []
     time.sleep(3)
-    for i in range(10):
-        data.append({'name': 'test{}'.format(i)})
 
-    return jsonify(data)
+    return jsonify(test_data)
 
 @app.route('/api/send_data', methods = ['POST'])
 def post_data():
---
+++ vegegrowth_ios/ViewModel/ViewerViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewerViewModel.swift
+//  vegegrowth_ios
+//
+//  Created by toui on 2023/07/16.
+//
+
+import Foundation
