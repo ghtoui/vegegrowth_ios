19197dac1ab3e8a509f9d34ce8358081b56edb51
ghtoui <92294389+ghtoui@users.noreply.github.com>
2023-07-17 13:28:42 +0900

    画像を含めた各種データを渡すAPI作成できた。 #22 テストとして、1, 2, 3, 4, 5の画像を渡す static/imagesに画像置いてある

--- APIdir/app/route.py
+++ APIdir/app/route.py
@@ -1,33 +1,36 @@
 from flask import jsonify, request
+import base64
 from app import app
 import time
 import datetime
 
-"""
-struct VegeLengthObject {
-    var date: String
-    var vegeLength: Double
-    var x: Double
-    var memoText: String
-}
-"""
+# このフォーマットは、swiftで指定したやつに合わせてる
+DATE_FORMAT = "%y年%m月%d日 %H時"
 
 # testデータ作成
 date = datetime.date(2023, 7, 1)
 data_length = 5
-date_format = "%Y年%m月%d日%H時"
+
+static_path = 'app/static/images/'
+image_list = ['one_2.jpg', 'two_2.jpg', 'three_2.jpg', 'four_2.jpg', 'five_2.jpg']
 
 name_list = ['レタス', 'キャベツ', 'きゅうり', 'なすび']
 date_list = []
 vegeLength_list = []
 x_list = []
 memoText_list = []
+encodeimage_list = []
 for i in range(data_length):
-    date_list.append(date.strftime(date_format))
+    date_list.append(date.strftime(DATE_FORMAT))
     vegeLength_list.append(float(i + 1.5))
     x_list.append(float(i))
     memoText_list.append("")
     date += datetime.timedelta(days = 1)
+    # 画像をエンコードする
+    with open(static_path + image_list[i], 'rb') as file:
+        image = file.read()
+        image_base64 = base64.b64encode(image).decode('utf-8')
+        encodeimage_list.append(image_base64)
 
 test_data = []
 for i in range(len(name_list)):
@@ -35,7 +38,8 @@ for i in range(len(name_list)):
             'date': date_list,
             'vegeLength': vegeLength_list,
             'x': x_list,
-            'memoText': memoText_list
+            'memoText': memoText_list,
+            'base64EncodedImage': encodeimage_list
             }
     test_data.append(data)
 
